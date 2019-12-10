2dd8bded9e49b3dcf98cf090d9b7ca82dba436f6
braianpipi <braian.arguello@hotmail.com>
2019-12-10 01:09:59 -0300

    Agregue los botones y una linea divisora para el footer

--- routes/main.js
+++ routes/main.js
@@ -7,5 +7,8 @@ const mainController = require('../controllers/mainController');
 
 /* GET - home page. */
 router.get('/', mainController.root);
-
+router.get("/Electricidad", mainController.root);
+router.get("/Plomeria", mainController.root);
+router.get("/Herramientas", mainController.root);
+router.get("/Construccion", mainController.root);
 module.exports = router;
